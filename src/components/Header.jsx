a2697dcef81295bec0e9999ba5a3874416948640
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-29 04:13:18 -0300

    responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { HashLink } from 'react-router-hash-link';
-import '../css/Header.css';
+// import '../css/Header.css';
 
 export default class Header extends Component {
   state = {  }
   render() {
     return (
       <header className="header-container">
-        <nav>
-          <HashLink classeName="nav-header-items" to="/#sobre">Sobre Mim</HashLink>
-          <HashLink classeName="nav-header-items" to="/#habilidades">Habilidades</HashLink>
-          <HashLink classeName="nav-header-items" to="/#projetos">Projetos</HashLink>
-          <HashLink classeName="nav-header-items" to="/#conteudo">Recomendações</HashLink>
-          <HashLink classeName="nav-header-items" to="">Contato</HashLink>
+
+        <input type="checkbox" id="control-nav" />
+        <label for="control-nav" class="control-nav"></label>
+        <label for="control-nav" class="control-nav-close"></label>
+
+        <nav className="float-r">
+          <ul className="list-auto">
+            <li>
+              <HashLink classeName="nav-header-items" to="/#sobre">Sobre Mim</HashLink>
+            </li>
+            <li>
+              <HashLink classeName="nav-header-items" to="/#habilidades">Habilidades</HashLink></li>
+            <li>
+              <HashLink classeName="nav-header-items" to="/#projetos">Projetos</HashLink>
+            </li>
+            <li>
+              <HashLink classeName="nav-header-items" to="/#conteudo">Recomendações</HashLink></li>
+            <li>
+              <HashLink classeName="nav-header-items" to="">Contato</HashLink>
+            </li>
+          </ul>
         </nav>
       </header>
     );
