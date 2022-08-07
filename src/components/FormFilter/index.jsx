a7208f606fe0771df0155ae87cd5ea47085caf3d
flaviosoliver <flavsoliver@gmail.com>
2022-08-07 11:26:57 -0300

    estrutura formfilter

---
+++ src/components/FormFilter/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function FormFilter() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="text-input">
+          <input
+            value={}
+            onChange={}
+            />
+        </label>
+        
+      </form>
+    </div>
+  )
+}
